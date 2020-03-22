791602ff445658b43cf1533526ab1e8f43d5495d
youittcom <youittcom@gmail.com>
2020-03-22 11:53:33 +0100

    estructura del index.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-echo "estas en el index.php";
-?>
+if(isset($_GET['controlador']) && $_GET['controlador'] != ''){
+    if($_GET['controlador'] == 'hola'){
+        echo "ok";
+    }
+}
+else{
+    require_once 'index.html';
+}
+
