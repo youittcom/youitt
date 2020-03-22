af7a8433a7ea2efd7c3d57ab38ce0e1ccc61a661
youittcom <youittcom@gmail.com>
2020-03-22 20:08:25 +0100

    conexion BBDD

---
+++ includes/conexion.php
@@ -0,0 +1,9 @@
+<?php
+//conexion a la Base de datos
+$server = 'localhost';
+$username = 'root';
+$password = 'UZJIvESy5x';
+$database = 'youitt';
+$db = mysqli_connect($server,$username,$password,$database);
+
+mysqli_query($db,"SET NAMES 'utf-8");
