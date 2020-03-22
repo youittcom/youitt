85b4587a41a56dff28800f505c4eede19c961afd
youittcom <youittcom@gmail.com>
2020-03-22 20:37:40 +0100

    Merge branch 'estructuraIndexPHP' into masterLocal
    # Conflicts:
    #	index.php

--- index.php
+++ index.php
@@ -1,11 +1,1 @@
-<?php
-if(isset($_GET['controlador']) && $_GET['controlador'] != ''){
-    $controlador = $_GET['controlador'].'Controller';
-    if(class_exists($controlador)){
-        echo "ok";
-    }
-}
-else{
-    require_once 'index.html';
-}
 
